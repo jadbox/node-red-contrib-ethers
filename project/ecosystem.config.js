9c5c8c6758f3557984f2166f5619a179ec57836e
Jonathan Dunlap <jdunlap@outlook.com>
2020-02-22 17:41:39 -0800

    Cleanup - refactor repo (#5)
    * refactor repo

---
+++ project/ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'ABI',
+    script: 'node-red',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: ["../node-red-contrib-ethers"],
+    ignore_watch : ["../node-red-contrib-ethers/node_modules"],
+    watch_options: {
+      "followSymlinks": true
+    },
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
